e2ea4639f8db7c66a72184920957c63d50b182c5
Burak Kalafat <burakkalafat@hotmail.com>
2019-04-02 23:35:20 +0300

    Create Store: Dispatch

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-function todos (state = [], action) {
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
-  }
-
-  return state
-}
-
-
-function createStore () {
+// Library Code
+function createStore (reducer) {
   // The store should have four parts
   // 1. The state
   // 2. Get the state.
@@ -25,9 +17,24 @@ function createStore () {
       listeners = listeners.filter((l) => l !== listener )
     }
   }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
   return {
     getState,
-    subscribe
+    subscribe,
+    dispatch,
   }
 }
 
+//App Code
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
+}
