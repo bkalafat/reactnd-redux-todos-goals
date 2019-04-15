6bc61799cf21ffb2a571672762182ff159af0b90
Burak Kalafat <burakkalafat@hotmail.com>
2019-04-15 21:24:14 +0300

    Goals Recuce

--- index.js
+++ index.js
@@ -36,13 +36,24 @@ function todos (state = [], action) {
   switch(action.type) {
     case 'ADD_TODO' :
       return state.concat([action.todo])
-    case 'REMOVE TODO' :
+    case 'REMOVE_TODO' :
       return state.filter((todo) => todo.id !== action.id)
     case 'TOGGLE_TODO' :
         return state.map(() => todo.id !== action.id ? todo :
           Object.assign({}, todo, {complete: !todo.complete}))
     default :
     return state
+  }
+}
+
+function goals (state = [], action) {
+  switch(action.type) {
+    case 'ADD_GOAL' :
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL' :
+      return state.filter((goal) => goal.id !== action.id)
+    default 
+      : return state
   }
 }
 
